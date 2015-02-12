234e5eea1a342d522938c3a5fe4e6910fe53fbda
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-02-12 09:51:01 -0500

    Top Navigation - I added a link for Events and now the navigation menu has pushed itself down - please adjust styling

--- assets/themes/exhibitionist/lib/js/homepage-jquery.js
+++ assets/themes/exhibitionist/lib/js/homepage-jquery.js
@@ -1,4 +1,5 @@
 var controller;
+
 jQuery(document).ready(function($) {
     var numwidgets = $('#homepage-widgets section.widget').length;
     $('#homepage-widgets').addClass('cols-'+numwidgets);
@@ -26,26 +27,59 @@ jQuery(document).ready(function($) {
 
     // show indicators (requires debug extension)
     //scene.addIndicators();
+    
+    function getBkgSize(img){
+    var result;
+    var imageSrc = $(img).css('backgroundImage')
+                       .replace(/url\((['"])?(.*?)\1\)/gi, '$2')
+                        .split(',')[0];    
+    var image = new Image();
+    image.src = imageSrc;
+    var bgwidth = image.width,
+        bgheight = image.height,    
+        bgContainWidth = $(img).width();
+    
+    var bgContainHeight = (bgheight*bgContainWidth)/bgwidth;
+    
+    var decimal = bgContainHeight.toString().split('.');
+    
+    if(decimal[0]>=5)
+    {
+        bgContainHeight = Math.ceil(bgContainHeight);
+    }
+    else
+    {
+        bgContainHeight = Math.floor(bgContainHeight);
+    }
+    result = {'height': bgContainHeight,'width':bgContainWidth};
+   return result;
+    
+}
+    
     function position_it(){
+        var bkgSize = getBkgSize($('#location .section-body'));
+        var imgWidth = bkgSize.width; //$('#location .section-body').width(); //checks out
+        var imgHeight = bkgSize.height;
+        var heightdiff = imgHeight - $('#location .section-body').height();
         
-        var width = $('#location .section-body').width();
-        var height = ($('#location .section-body').width()*1156)/1389;
-        var heightdiff = height - $('#location .section-body').height();
         
-        $('#locations_popovers').css('left', '50%').css('top',(height-heightdiff)/2 + 'px');
+        $('#locations_popovers').css('left', '50%').css('top',(imgHeight-heightdiff)/2 + 'px').css('border','1px solid red');
         for(var i = 0, l = location_positions.length; i < l; ++i){
             var elem = location_positions[i].elem;
             var posstr = location_positions[i].position;
+            //console.log(elem + ': ' + posstr);
             var pos = posstr.split("|");
-            var left = ((pos[0]*(width/2))-($(elem).width()/2));
-            var top = ((pos[1]*(height/2))+($(elem).height()));
+            var left = ((pos[0]*(imgWidth/2))-($(elem).width()/2)); //checks out
+            var top = ((pos[1]*(imgHeight/2))+($(elem).height()));
+            
             console.log(elem + ': ' + left + '|' + top);
             $(elem).css("left",left + "px").css("top",top + "px");
             var btm = $(elem).css("bottom");
             $(elem).css("bottom",btm).css("top","auto");
         }
     }
-    
+    var testbox = '<div style="height: 100px; width: 100px; background-color: red; opacity: 0.5;position: absolute;top: 50%; left: 50%;"></div>';
+    $('#locations_popovers').append(testbox);
     position_it();
     $('a.map-marker').hover(function(e){
     });
