3f35d9e10830b3f5f8a581ffb86a7535b8421432
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-03-04 20:57:17 -0500

    i think i finally got the map working!!!!

--- assets/themes/exhibitionist/lib/js/homepage-jquery.js
+++ assets/themes/exhibitionist/lib/js/homepage-jquery.js
@@ -55,25 +55,47 @@ jQuery(document).ready(function($) {
    return result;
     
 }
+    function original_image_size(elem){
+        var image_url = elem.css('background-image'),
+            image;
+        
+        // Remove url() or in case of Chrome url("")
+        image_url = image_url.match(/^url\("?(.+?)"?\)$/);
+        
+        if (image_url[1]) {
+            image_url = image_url[1];
+            image = new Image();
+        
+            // just in case it is not already loaded
+            $(image).load(function () {
+                console.log(image.width + 'x' + image.height);
+                return([{width:image.width,height:image.height}]);
+            });
+        
+            //return([{width:image.width,height:image.height}]);
+            image.src = image_url;
+        }
+    }
     
     function position_it(){
-        var bkgSize = getBkgSize($('#location .section-body'));
-        var imgWidth = bkgSize.width; //$('#location .section-body').width(); //checks out
-        var imgHeight = bkgSize.height;
+        //var bkgSize = original_image_size($('#location .section-body'));
+        var bkgSize = {width:1389,height:1156};
+        var currBkgSize = getBkgSize($('#location .section-body'));
+        var imgWidth = currBkgSize.width; //$('#location .section-body').width(); //checks out
+        var imgHeight = currBkgSize.height;
+        var widthMultiplier = currBkgSize.width/bkgSize.width;
+        var heightMultiplier = currBkgSize.height/bkgSize.height;
         var heightdiff = imgHeight - $('#location .section-body').height();
         var windowsize = $(window).width();
-        
-        if(windowsize > 786){
+        if(windowsize > 768){
             $('#locations_popovers').css('left', '50%').css('top',(imgHeight-heightdiff)/2 + 'px').css('border','1px solid red');
             for(var i = 0, l = location_positions.length; i < l; ++i){
                 var elem = location_positions[i].elem;
                 var posstr = location_positions[i].position;
-                //console.log(elem + ': ' + posstr);
                 var pos = posstr.split("|");
-                var left = ((pos[0]*(imgWidth/2))-($(elem).width()/2)); //checks out
-                var top = ((pos[1]*(imgHeight/2))+($(elem).height()));
+                var left = ((pos[0]*widthMultiplier)-($(elem).width()/2));
+                var top = ((pos[1]*heightMultiplier)-($(elem).height()));
                 
-                console.log(elem + ': ' + left + '|' + top);
                 $(elem).css("left",left + "px").css("top",top + "px");
                 var btm = $(elem).css("bottom");
                 $(elem).css("bottom",btm).css("top","auto");
@@ -81,7 +103,7 @@ jQuery(document).ready(function($) {
         }
     }
     var testbox = '<div style="height: 100px; width: 100px; background-color: red; opacity: 0.5;position: absolute;top: 50%; left: 50%;"></div>';
-    //$('#locations_popovers').append(testbox);
+    $('#locations_popovers').append(testbox);
     position_it();
     $('a.map-marker').hover(function(e){
     });
