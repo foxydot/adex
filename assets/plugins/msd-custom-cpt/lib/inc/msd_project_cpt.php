621dab426e65e0855f1dd314c2d9e11a0c585e0f
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-05-14 14:05:12 -0400

    fix sortable error so url can be cahnged

--- assets/plugins/msd-custom-cpt/lib/inc/msd_project_cpt.php
+++ assets/plugins/msd-custom-cpt/lib/inc/msd_project_cpt.php
@@ -157,6 +157,7 @@ if (!class_exists('MSDProjectCPT')) {
                 wp_enqueue_script('thickbox');
                 //wp_register_script('my-upload', plugin_dir_url(dirname(__FILE__)).'/js/msd-upload-file.js', array('jquery','media-upload','thickbox'),FALSE,TRUE);
                 wp_enqueue_script('my-upload');
+                wp_enqueue_script( 'jquery-ui-sortable' );
             }
         }
         
